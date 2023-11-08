01cc59e6f2534ae0ec9c9a7a0c5bff9abac4f4f8
Satyendra Kumar <ksatyendra218@gmail.com>
2023-11-08 15:49:20 +0530

    Updated DigiLib_UI_Deploy.sh script to get rid of repo under Sharath and refer to DigiLib Org

--- DigiLib_UI_Deploy.sh
+++ DigiLib_UI_Deploy.sh
@@ -1,11 +1,11 @@
 cd /home/ec2-user/
 rm -R -f build/
-wget https://github.com/sharathkumbar1/DigitalLibrary/archive/refs/heads/master.zip
-unzip master.zip
-mv DigitalLibrary-master/build/ /home/ec2-user/
+wget https://github.com/digilib-samarthanam/digilibrary-ui/archive/refs/heads/main.zip
+unzip main.zip
+mv digilibrary-ui-main/build/ /home/ec2-user/
 chown -R root:root build
-mv -f DigitalLibrary-master/nginx.conf /etc/nginx/
+mv -f digilibrary-ui-main/nginx.conf /etc/nginx/
 chown -R root:root /etc/nginx/nginx.conf
-rm -R -f DigitalLibrary-master/
+rm -R -f digilibrary-ui-main/
 rm -f master.zip
 service nginx restart
